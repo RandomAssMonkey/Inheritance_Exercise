c9628ec550a8757e32444fc4193393f515f8f797
Petrik Diak <diak@petrik.hu>
2023-10-03 10:31:51 +0200

    Person class

---
+++ Inheritance_Exercise/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Exercise
+{
+	internal class Person
+	{
+		private string name;
+		private int age;
+		private string gender;
+
+		public string Name { get => name; set => name = value; }
+		public int Age { get => age; set => age = value; }
+		public string Gender { get => gender; set => gender = value; }
+
+		public Person(string name, int age, string gender)
+		{
+			this.name = name;
+			this.age = age;
+			this.gender = gender;
+		}
+
+		public Person() 
+		{
+			this.name = "Jane Doe";
+			this.age= 30;
+			this.gender = "croissant";
+		}
+
+		public string GetGoal()
+		{
+			return "My goal is: Live for the moment";
+		}
+
+		public override string ToString()
+		{
+			return $"Hi, I'm {this.name}, a {this.age} year old {this.gender}";
+		}
+	}
+}
