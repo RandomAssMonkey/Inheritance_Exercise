a512301a330aee94d0c838d6acb5dde29da77238
Petrik Diak <diak@petrik.hu>
2023-10-03 10:46:07 +0200

    Student class Constructor

--- Inheritance_Exercise/Student.cs
+++ Inheritance_Exercise/Student.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Inheritance_Exercise
 {
-	internal class Student
+	internal class Student: Person
 	{
+		private string previousOrganization;
+		private int skippedDays;
+
+		public Student(string previousOrganization, string name, int age, string gender): base(name, age, gender)
+		{
+			this.previousOrganization = previousOrganization;
+			this.skippedDays = 0;
+		}
+		public Student():base("Jane Doe", 30, "Croissant")
+		{
+			this.previousOrganization = "School of Life";
+			this.skippedDays = 0;
+		}
 	}
 }
